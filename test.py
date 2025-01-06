229db2604c25ad72880a883008daa35ff8699b04
Blesson K Tomy <116932677+Blesson-Tomy@users.noreply.github.com>
2025-01-07 00:01:32 +0530

    added fib

--- test.py
+++ test.py
@@ -1,1 +1,15 @@
-print('hello')
+#print('Hello. First python lesson in college.')
+
+#for i in range(0,100,10):
+    #print(i)
+
+#fibonocci series
+a =0
+b =1
+print(a)
+print(b)
+for i in range(0,10,1):
+    temp=a
+    a=b
+    b=temp+b
+    print(b)
