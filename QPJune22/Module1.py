1447dc7b51462cf9c0808ee06a15d8229b7d2c5a
Blesson K Tomy <116932677+Blesson-Tomy@users.noreply.github.com>
2025-02-02 23:55:40 +0530

    py qns

---
+++ QPJune22/Module1.py
@@ -0,0 +1,23 @@
+"""
+#Qn1) 
+print(9/2)
+print(9//2)
+"""
+
+"""
+# Qn2)
+set = {1,2,3,4,5,6,7,8,9,11}
+even=0
+odd=0
+print("Length of Set:",len(set))
+for i in set:
+    if (i%2==0):
+        even=even+1
+    else:
+        odd=odd+1
+
+print("The number of odd numbers in the set is: ",odd)
+print("The number of even numbers in the set is: ",even)
+"""
+
+
--- workbook.py
+++ workbook.py
@@ -12,7 +12,7 @@ String2 = "is hot"
 
 String+= String2
 print(String[0:3])
-"""
+
 
 x = "Aromal and Tom and Blesson is part of the Class Rep Gang"
 #spl = x.split()
@@ -25,3 +25,21 @@ print(finde)
 #To reverse a string using the slice function
 x = "Blesson" [::-1]
 print(x)
+"""
+
+list = ["Aswin MS", "Tom Sibus", "Alan Joy", "Nikki Tom","Nikki Tom", "Small Elephant"]
+#r=set(list)
+"""
+r=enumerate(list)
+
+for i, word in r:
+    print(i,word)
+    
+x=sorted(list,reverse=True)
+"""
+x=sorted(list,key=len)
+print(x)
+
+
+#Program to read a list of names and sort in alphabetic order
+#Program to remove all duplicates form a list
