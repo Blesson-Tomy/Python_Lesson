c5d1ce53f8cf9022dc458e061dc0dd3e0f7ec150
Blesson-Tomy <blessonsmac@gmail.com>
2025-05-13 00:37:57 +0530

    updates QPJan24

---
+++ QPJan24/Module2.py
@@ -0,0 +1,49 @@
+#Qn4) Write a program to identify palindromic words in lines of text.
+
+def pal(word):
+    word=word.lower()
+    return word==word[::-1]
+
+f1=open("books.txt","r+")
+
+f=f1.read()
+
+f=f.split()
+palindrome=set()
+
+for word in f:
+    word=''.join(filter(str.isalpha,word))
+    if len(word)>1 and pal(word):
+        palindrome.add(word)
+
+for i in sorted(palindrome):
+    print(i)
+
+#Qn14a) Decimal Number to binary equivalent
+"""
+dec=int(123)
+res=''
+
+while dec!=0:
+    temp=dec%2
+    res=str(temp)+res
+    dec=dec//2
+
+print(res)
+"""
+
+#14b) Read from file and count occurances and store into a dictionary
+"""
+f1=open("books.txt","r+")
+dicti={}
+f=f1.readlines()
+for line in f:
+    for ch in line.upper():
+        if ch in dicti:
+            dicti[ch]=dicti[ch]+1
+        else:
+            dicti[ch]=1
+
+print(dicti)
+"""
+
--- QPJan24/Module4.py
+++ QPJan24/Module4.py
@@ -31,7 +31,7 @@ finally:
 """
 
 #Qn17a) Illustrate the use of Abstract Classes in Python
-
+"""
 from abc import ABC,abstractmethod
 
 class absc(ABC):
@@ -56,3 +56,63 @@ c=cat()
 c.hands(10)
 a=absc()
 a.hands(3)
+"""
+
+#Qn17b) Define a class Student .
+"""
+class Student():
+    rollno=0
+    name=''
+    math=0
+    sci=0
+    el=0
+
+    def ReadD(self,rollno,name,math,sci,el):
+        self.rollno=rollno
+        self.name=name
+        self.math=math
+        self.sci=sci
+        self.el=el
+
+    def compute(self):
+        tmarks=self.math+self.sci+self.el
+        print(tmarks)
+    
+    def printt(self):
+        print(self.rollno, self.name,self.math,self.sci,self.el,)
+
+
+a=Student()
+a.ReadD(11,'Blesson',100,100,99)
+a.printt()
+
+"""
+
+#Qn14a) Decimal Number to binary equivalent
+"""
+dec=int(123)
+res=''
+
+while dec!=0:
+    temp=dec%2
+    res=str(temp)+res
+    dec=dec//2
+
+print(res)
+"""
+
+#14b) Read from file and count occurances and store into a dictionary
+"""
+f1=open("books.txt","r+")
+dicti={}
+f=f1.readlines()
+for line in f:
+    for ch in line.upper():
+        if ch in dicti:
+            dicti[ch]=dicti[ch]+1
+        else:
+            dicti[ch]=1
+
+print(dicti)
+"""
+
